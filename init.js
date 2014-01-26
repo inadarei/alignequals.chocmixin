9cffe5365d5984d9b40732fbfcbe39fff607736c
Irakli Nadareishvili <irakli@gmail.com>
2014-01-25 23:44:24 -0500

    Create init.js

---
+++ init.js
@@ -0,0 +1,51 @@
+/*!
+ * Align Equal Signs. A mixin for Chocolat
+ * Copyright(c) 2014 Irakli Nadareishvili
+ * MIT Licensed
+ *
+ * @see: http://chocolatapp.com/blog/?chocolat-node
+ * @see: http://mixins.chocolatapp.com/docs/
+ */
+
+Hooks.addMenuItem('Actions/Javascript/JSON/Clean Formatting', 'cmd-alt-j', function() {
+	Recipe.run(function(recipe) {
+		
+		var sel = (!recipe.selection.length)? new Range(0, recipe.length) : recipe.selection
+			, output = ''
+			, text = recipe.textInRange(sel);
+			
+		recipe.replaceTextInRange(sel, alignEquals(text));
+	});
+});
+
+
+function alignEquals (text) {
+text = text.replace(/(.+?)\s+?=\s+(.*)/gm, "$1 = $2");
+
+var lines = text.split("\n");
+var positions = [];
+var maxPos = 0;
+for (var i=0; i<lines.length; i++) {
+  positions[i] = lines[i].indexOf("=");
+  if (positions[i] > maxPos) {
+    maxPos = positions[i];
+  }
+}
+
+
+for (var i=0; i<lines.length; i++) {
+  if (positions[i] > 0) {
+    var diff = maxPos - positions[i];
+    var newline = lines[i].substr(0, positions[i] - 1);
+    var spaces = "";
+    for (var j=0; j<=diff; j++) {
+      spaces += " ";
+    }
+    newline += spaces;
+    newline += lines[i].substr(positions[i]);
+    lines[i] = newline;
+  }
+}
+
+return lines.join("\n");
+}
